f51ea144b02c207e8142b762dcd1121acd65a37d
jcode <746026935@qq.com>
2022-06-03 16:28:09 +0800

    feat: 增加vuex

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+// 创建一个新的 store 实例
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state, payload) {
+      state.count += payload
+    }
+  }
+})
+
+export default store
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
 
 const count = ref(1)
+
+const add = () => {
+  store.commit('increment', 3)
+}
 
 </script>
 
 <template>
   index
   {{ count }}
+  {{ store.state.count }}
+  <button @click="add">
+    增加
+  </button>
 </template>
 
 <style scoped>
